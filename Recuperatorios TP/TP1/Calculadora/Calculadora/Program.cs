76fc32e5c043ea4050e93f202ac8e23039d12bfc
Enrique <mendioroze@gmail.com>
2021-11-08 18:58:07 -0300

    recuperatorio TP1

---
+++ Recuperatorios TP/TP1/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Entidades;
+
+
+
+namespace MiCalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Operando operando1 = new Operando();
+            operando1.Numero = "9";
+
+
+            Operando operando2 = new Operando("4");
+
+            Console.WriteLine(Calculadora.Operar(operando1, operando2, '*'));
+
+            Console.WriteLine(operando1.BinarioDecimal("111"));
+            Console.WriteLine(operando1.DecimalBinario(126.774));
+
+     
+        }
+    }
+}
